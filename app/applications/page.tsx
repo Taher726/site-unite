bd7aa0178a2ee30f6738d5ea78e1bdcf3b390469
Taher726 <chaltouttaher@gmail.com>
2025-07-07 16:25:11 +0100

    Change case study to applications

---
+++ app/applications/page.tsx
@@ -0,0 +1,9 @@
+import HeroApplications from "./components/hero_applications";
+
+export default function ApplicationsPage() {
+    return(
+        <div className="bg-white">
+            <HeroApplications/>
+        </div>
+    );
+}
